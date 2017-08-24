d69bda47e401ce119c0e481bffb1f8c75227f9a7
Cyril PHAM-LE <cyrilphamle@gmail.com>
2017-08-24 20:05:09 +0200

    Add : having total deposit as current balance when making multiple deposits

--- src/test/java/fr/bank/account/AccountTest.java
+++ src/test/java/fr/bank/account/AccountTest.java
@@ -1,5 +1,6 @@
 package fr.bank.account;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.math.BigDecimal;
@@ -8,11 +9,17 @@ import java.math.MathContext;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AccountTest {
-    /*
+  private Account account;
+  /*
     - As a bank client
     - I want to make a deposit in my account
     - In order to save money
      */
+
+  @Before
+  public void setUp() throws Exception {
+    account = new Account();
+  }
 
   @Test
   public void should_have_zero_if_no_deposit_has_been_made() throws Exception {
@@ -22,8 +29,14 @@ public class AccountTest {
 
   @Test
   public void should_have_amount_when_making_a_deposit() throws Exception {
-    Account account = new Account();
-    account.deposit(new BigDecimal(100));
+    account.deposit(new BigDecimal(100, MathContext.DECIMAL64));
     assertThat(account.getCurrentBalance()).isEqualTo(new BigDecimal(100, MathContext.DECIMAL64));
+  }
+
+  @Test
+  public void should_have_total_deposit_when_making_multiple_deposits() throws Exception {
+    account.deposit(new BigDecimal(100, MathContext.DECIMAL64));
+    account.deposit(new BigDecimal(200, MathContext.DECIMAL64));
+    assertThat(account.getCurrentBalance()).isEqualTo(new BigDecimal(300, MathContext.DECIMAL64));
   }
 }
